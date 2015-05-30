0258631acbc0207f2f7fac46557123b8ad40469f
Logan Wright <logan.william.wright@gmail.com>
2015-05-29 21:34:00 -0400

    Initial Commit

---
+++ PolymerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PolymerSwift
+//
+//  Created by Logan Wright on 5/29/15.
+//  Copyright (c) 2015 lowriDevs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
